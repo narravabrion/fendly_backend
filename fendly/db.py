0c2ba4a1933c54654ce6e32f3b5a4283c8d2adfe
narravabrion <kidbrion7@gmail.com>
2022-05-19 09:38:15 -0700

    postgres

--- fendly/db.py
+++ fendly/db.py
@@ -1,45 +1,25 @@
 import sqlite3
 import os
-import click 
 from flask import current_app,g
 from flask.cli import with_appcontext
+import psycopg2
 
-def connect_to_db():
-    sql = sqlite3.connect(os.path.join(os.path.dirname(__file__),'fendly.db'))
-    # sql = sqlite3.connect('D:/skaehub/fendly/fendly_backend/fendly/fendly.db')
-    sql.row_factory = sqlite3.Row
-    return sql
-
-def get_db():
-    if 'fendly' not in g:
-        g.fendly=connect_to_db()
-    return g.fendly
+# def connect_to_db():
+#     sql = sqlite3.connect(os.path.join(os.path.dirname(__file__),'fendly.db'))
+#     sql.row_factory = sqlite3.Row
+#     return sql
 
 # def get_db():
-#     if 'db' not in g:
-#         g.db=sqlite3.connect(
-#             current_app.config['DATABASE'],
-#             detect_types=sqlite3.PARSE_DECLTYPES
-#         )
-#         g.db.row_factory =sqlite3.Row
-#         return g.db
-
-# def close_db(e=None):
-#     db=g.pop('db',None)
-#     if db is not None:
-#         db.close()
-
-# def init_db():
-#     db = get_db()
-
-#     with current_app.open_resource('schema.sql') as f:
-#         db.executescript(f.read().decode('utf8'))
+#     if 'fendly' not in g:
+#         g.fendly=connect_to_db()
+#     return g.fendly
 
 
-# @click.command('init-db')
-# @with_appcontext
-# def init_db_command():
-#     # Clear the existing data and create new tables.
-#     init_db()
-#     click.echo('Initialized the database.')
-
+def get_db():
+    sql = psycopg2.connect(user="xmetbafaylxgso",
+                                  password="dd70034b30be181c4262a8ed872f0becfdd6c1c7f187a8abcc3e62b85b71528b",
+                                  host="ec2-3-231-82-226.compute-1.amazonaws.com",
+                                  port="5432",
+                                  database="de170uk9i89kkn")
+    # curr = sql.cursor()
+    return sql
--- fendly/schema.sql
+++ fendly/schema.sql
@@ -1,15 +1,13 @@
-
-CREATE TABLE IF NOT EXISTS user(
-    userID INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS "user" (
+    userID INTEGER PRIMARY KEY generated always as identity,
     first_name CHAR(45) NOT NULL,
     last_name CHAR(45) NOT NULL,
     username CHAR(45) NOT NULL,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS user_details(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS "user_details" (
+    id INTEGER PRIMARY KEY generated always as identity,
     userID INTEGER UNIQUE NOT NULL,
     profile_pic TEXT NOT NULL,
     job_title TEXT NOT NULL,
@@ -18,5 +16,5 @@ CREATE TABLE IF NOT EXISTS user_details(
     twitter_username TEXT NOT NULL,
     github_username TEXT NOT NULL,
     website TEXT NOT NULL,
-    FOREIGN KEY (userID)  REFERENCES user(userID) ON DELETE CASCADE ON UPDATE CASCADE 
+    FOREIGN KEY (userID) REFERENCES "user" (userID) ON DELETE CASCADE ON UPDATE CASCADE
 );
